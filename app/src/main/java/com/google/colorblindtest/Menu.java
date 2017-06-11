3e9cc29607fd63e55ab04b0ff697323d802e2faf
riniwijayanti <rini7972@students.amikom.ac.id>
2017-06-11 20:34:02 +0700

    commit menu

--- app/src/main/java/com/google/colorblindtest/Menu.java
+++ app/src/main/java/com/google/colorblindtest/Menu.java
@@ -1,13 +1,27 @@
 package com.google.colorblindtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        start = (Button) findViewById(R.id.btnStart);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Kuis.class));
+            }
+        });
     }
 }
